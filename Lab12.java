066d8d5a303456dc1e132507112a4dda61ce90f2
LampAtmosphere <122746059+LampAtmosphere@users.noreply.github.com>
2023-04-19 20:26:05 +0300

    комит1

--- Lab12.java
+++ Lab12.java
@@ -5,17 +5,14 @@
     public class Lab12 {
         public static void main(String[] args) {
             String text = "Some text with an IP address 192.168.0.1 and another one 10.0.0.1"; // Заданный текст для поиска IP-адреса
-    
-            // Шаблон регулярного выражения для поиска IP-адреса
+            
+           
             String patternString = "(\\d{1,3}\\.){3}\\d{1,3}";
     
-            // Создание объекта Pattern
             Pattern pattern = Pattern.compile(patternString);
-    
-            // Поиск совпадений в тексте
+          
             Matcher matcher = pattern.matcher(text);
     
-            // Вывод всех найденных IP-адресов
             while (matcher.find()) {
                 System.out.println(matcher.group());
             }
